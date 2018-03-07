2cc6e48e7036f7f2ce2a834abdfa032adfee8e16
student <student@pc-202-02>
2018-03-07 18:01:04 +0100

    Zadanialab

--- POLab2/src/pl/edu/ur/main/Main.java
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -1,5 +1,7 @@
 package pl.edu.ur.main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marcin
@@ -11,6 +13,85 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO zad 3, 4, 6, 7
+        
+        Scanner input = new Scanner(System.in);
+        int liczba = input.nextInt();
+        {
+        while (liczba !=0){
+        switch (liczba) {
+            case 1:
+                System.out.println("Styczeń");
+                break;
+            case 2:
+                System.out.println("Luty");
+                break;
+            case 3:
+                System.out.println("Marzec");
+                break;
+            case 4:
+                System.out.println("Kwiecień");
+                break;
+            case 5:
+                System.out.println("Maj");
+                break;
+            case 6:
+                System.out.println("Czerwiec");
+                break;
+            case 7:
+                System.out.println("Lipiec");
+                break;
+            case 8:
+                System.out.println("Sierpień");
+                break;
+            case 9:
+                System.out.println("Wrzesień");
+                break;
+            case 10:
+                System.out.println("Październik");
+                break;
+            case 11:
+                System.out.println("Listopad");
+                break;
+            case 12:
+                System.out.println("Grudzień");
+                break;
+            default:
+                System.out.println("Błedny numer");
+                break;
+        }
+
     }
-    
-}
+        int liczba1=10;
+        
+        int c=0;
+                
+        int iteracja1=0;
+        int iteracja2=0;
+        while (c<1000) {
+            c += liczba1;
+            iteracja1++;
+        }
+        System.out.println(iteracja1);
+           c =0;
+        do {
+             c += liczba1;
+            iteracja2++;
+        }
+        while(c<1000);
+        System.out.println(iteracja2);
+        }
+        
+        for (int i=0; i<20; i++)
+        {
+            if (i==2 || i==6 || i==9 || i==15 || i==19)
+            {        continue;
+           
+          
+            }
+         
+            System.out.println(i);
+        }
+        
+        
+       
+}}
